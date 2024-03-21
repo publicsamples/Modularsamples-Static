c547cf2bb43d9c3e23cab82e5e3998f0e8327b12
Rick <53375860+RickTaylor79@users.noreply.github.com>
2024-03-21 15:00:43 +0000

    Q DEMO

---
+++ Demos/presets/Quetzalcoatl/file1.js
@@ -0,0 +1,25 @@
+var keys = new Tone.Sampler({
+		
+			"C1" : "file1.[mp3|ogg]",
+	        "E1" : "file2.[mp3|ogg]",
+	        "G#1" : "file3.[mp3|ogg]",
+			"C2" : "file4.[mp3|ogg]",
+			"E2" : "file5.[mp3|ogg]",
+			"G#2" : "file6.[mp3|ogg]",
+			"C3" : "file7.[mp3|ogg]",
+			"E3" : "file8.[mp3|ogg]",
+			"G#3" : "file9.[mp3|ogg]",
+			"C4" : "file10.[mp3|ogg]",
+			"E4" : "file11.[mp3|ogg]",
+			"G#4" : "file12.[mp3|ogg]",
+			"C5" : "file13.[mp3|ogg]",
+	        "E5" : "file14.[mp3|ogg]",
+	        "G#5" : "file15.[mp3|ogg]",
+			"C6" : "file16.[mp3|ogg]"
+				
+				
+					}, {
+
+						"baseUrl" : "/Demos/presets/Quetzalcoatl/Atlantean/"
+					})					.connect(filter);
+					
